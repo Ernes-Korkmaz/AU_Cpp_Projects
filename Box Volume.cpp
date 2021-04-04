3def9425a0094f68b3a2b7940ab7622155d21385
Ernes <ErnesKorkmaz105@gmail.com>
2021-04-04 21:16:53 +0300

    BoxVolume Added

---
+++ Box Volume.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+
+int BoxVolume(int length=1, int width=1, int height=1);
+
+int main()
+{
+	cout<<"The default volume is : "<<BoxVolume()<<endl;
+	
+	cout<<" Volume of a box with int length=10, int width=1, int height=1 is :  "<<BoxVolume(10)<<endl;
+	
+	cout<<" Volume of a box with int length=10, int width=5, int height=1  is : "<<BoxVolume(10,5)<<endl;
+	
+	cout<<" Volume of a box with int length=10, int width=1, int height=3 is :  "<<BoxVolume(10,5,3)<<endl;	
+	
+	return 0;
+}
+
+int BoxVolume(int length, int width, int height)
+{
+	return length*width*height;
+}
